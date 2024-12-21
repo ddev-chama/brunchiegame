cade37d959f93e33ff0041f9fba6aad53b72c221
Pradit Yamsook <praditcry@gmail.com>
2024-12-22 06:29:59 +0700

    [FIX]: add try catch purchase use effect

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -33,9 +33,13 @@ const Login: React.FC = () => {
           history.replace('/login');
       }
       (async () => {
-        await Purchases.setLogLevel({ level: LOG_LEVEL.DEBUG });
-        await Purchases.configure({ apiKey: import.meta.env.VITE_API_KEY_PURCHASE });
-        await Purchases.setSimulatesAskToBuyInSandbox({ simulatesAskToBuyInSandbox: true });
+        try {
+            await Purchases.setLogLevel({ level: LOG_LEVEL.DEBUG });
+            await Purchases.configure({ apiKey: import.meta.env.VITE_API_KEY_PURCHASE });
+            await Purchases.setSimulatesAskToBuyInSandbox({ simulatesAskToBuyInSandbox: true }); 
+        } catch (error) {
+            console.error("Purchases error:", error);
+        }
       });
   }, []);
   
