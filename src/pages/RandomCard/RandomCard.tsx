e01927d9d241ced23908aead3c6e70e3b96dc799
Chama kongsang <ddev.chama@gmail.com>
2026-02-25 17:33:25 +0700

    Remove 18+ category from random mode question files in RandomCard component

--- src/pages/RandomCard/RandomCard.tsx
+++ src/pages/RandomCard/RandomCard.tsx
@@ -92,7 +92,8 @@ const RandomCard: React.FC = () => {
   const loadMultipleTextContent = async () => {
     setLoading(true);
     const allLines: { text: string, iconFile: string }[] = [];
-    const questionFiles = ['question1', 'question2', 'question3', 'question4', 'question7'];
+    /* ซ่อนหมวด 18+ ใน random mode */
+    const questionFiles = ['question1', 'question2', 'question3', 'question4'];
 
     const questionFileLines: { [key: string]: { text: string, iconFile: string }[] } = {};
 
